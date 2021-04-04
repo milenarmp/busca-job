32309ccadb858eefad3a7aa12ad2d7c1b09cfeeb
Milena Rodrigues <milena.rodrigues@becompliance.com>
2021-04-04 00:50:37 -0300

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<v-toolbar
+			dark
+			color="#102947"
+		>
+			<v-spacer></v-spacer>
+			<v-toolbar-title class="titlePage mt-10">Busca Job</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn icon>
+				<v-icon>mdi-export</v-icon>
+			</v-btn>
+			<template v-slot:extension>
+
+			</template>
+		</v-toolbar>
+		<v-tabs
+			v-model="tab"
+			align-with-title
+			mobile-breakpoint="450"
+		>
+			<v-tabs-slider color="#D2BFB8"></v-tabs-slider>
+
+			<v-tab class="tabItem" to="/">Achar vagas</v-tab>
+			<v-tab class="tabItem" to="/cadastrar-vaga">Cadastrar vagas</v-tab>
+		</v-tabs>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+	data() {
+		return {
+			tab : null
+		}
+	}
+}
+</script>
+
+<style scoped>
+* {
+	font-family: 'Montserrat', sans-serif;
+}
+
+.titlePage {
+	font-size: 3.5rem !important;
+	font-family: 'Montserrat', sans-serif;
+	text-align: center;
+}
+
+.tabItem {
+	font-size: 1rem;
+}
+</style>
---
+++ src/views/SearchJob.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+		<v-row>
+			<v-col>
+				<v-card class="mt-8" color="#D2BFB8">
+					<v-row justify="center">
+						<v-col align-self="center" class="mt-8" sm="8" cols="10" lg="8" md="8">
+							<v-text-field
+								solo
+								placeholder="Busque uma oportunidade aqui..."
+								prepend-inner-icon="mdi-magnify"
+								hint="Professor, desenvolvedor, gerente, etc..."
+							></v-text-field>
+						</v-col>
+						<v-col align-self="center" cols="10" sm="2" lg="2" md="2">
+							<v-btn
+								elevation="2"
+								large
+								color="#102947"
+								dark
+							>
+								<v-icon
+									dark
+								>
+									mdi-magnify
+								</v-icon>				
+								Buscar
+							</v-btn>
+						</v-col>
+					</v-row>
+				</v-card>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col>
+				<!-- TODO: VAGAS MAIS NOVAS -->
+			</v-col>
+		</v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+</style>
